d7e5b07df5aa1ce831a7b4d14261e6176151440b
XTorake <34582369+XTorake@users.noreply.github.com>
2021-01-15 15:58:02 -0600

    Resueltos ejercicios 2 y 3

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+/*
+    La primera línea de la entrada da el número de casos de prueba, T. 
+    Luego siguen T casos de prueba, uno por línea.
+    Cada línea se compone de una cadena G en Googlerense, 
+    compuesto por una o más palabras que contengan las letras de la 'a' a la 'z'. 
+    Habrá exactamente un espacio entre aplabras consecutivas 
+    y no habrá espacios al comienzo o al final de una línea.
+
+ ENTRADA DE EJEMPLO
+    3
+
+    ejp mysljylc kd kxveddknmc re jsicpdrysi
+
+    rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd
+
+    de kr kd eoya kw aej tysr re ujdr lkgc jv
+
+SALIDA DE EJEMPLO
+
+    Case #1: our language is impossible to understand
+
+    Caso #2: there are twenty six factorial possibilities
+
+    Caso #3: so it is okay if you want to just give up
+
+*/
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la cantidad de casos de uso");
+            string numCasos = Console.ReadLine();
+            string traducido = "";
+
+            for (int i = 0; i < Convert.ToInt32(numCasos); i++)
+            {
+                Console.WriteLine("Ingresa la cadena a traducir");
+                string input = Console.ReadLine();
+                if (input.Length < 100)
+                {
+                    traducido = "Case #" + (i + 1) + ":" + Replace(input);
+                }
+                else
+                {
+                    while (input.Length > 100)
+                    {
+                        Console.WriteLine("La cadena no puede se mayor a 100 caracteres\n");
+                        Console.WriteLine("Ingresa la cadena a traducir");
+                        input = Console.ReadLine();
+                    }
+                    traducido = "Case #" + (i + 1) + ": " + Replace(input)+"\n";
+                }
+
+                Console.WriteLine(traducido);
+            }
+        }
+        public static string Replace(string input)
+        {
+            char[] key = { 'y', 'h', 'e', 's', 'o', 'c', 'v', 'x', 'd', 'u', 'i', 'g', 'l', 'b', 'k', 'r', 'z', 't', 'n', 'w', 'j', 'p', 'f', 'm', 'a', 'q' };
+            char[] abc = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            char[] cadena = input.ToCharArray();
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i] != ' ')
+                {
+                    int index = Array.FindIndex(abc, x => x.Equals(cadena[i]));
+                    cadena[i] = key[index];
+                }
+            }
+            input = new string(cadena);
+            return input;
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+/*
+* Un entero se dice que es un palíndromo si es igual al número que se obtiene al invertir el orden de sus cifras.
+* Por ejemplo, 79197 y 324423 son palíndromos. 
+* 
+* En esta tarea se le dará un entero N, 1 <= N <= 1.000.000 
+* Usted debe encontrar el menor entero M tal que M >= N M es primo y es un palíndromo.
+* 
+* *Por ejemplo, si N es 31, entonces la respuesta es 101.
+* 
+* Formato de entrada:
+* Un solo entero N, (1 <= N <= 1.000.000), en una sola línea.
+* 
+* Formato de salida:
+* Su salida debe consistir en un solo número entero, el más pequeño palíndromo primo mayor que o igual a N.
+*
+*/
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Operacion(Console.ReadLine()));
+        }
+
+
+
+        public static bool Primo(int num)
+        {
+            int divisor = 2;
+            while (divisor <= num)
+            {
+
+                if (num % divisor == 0)
+                {
+
+                    if (divisor != num)
+                    {
+                        return false;
+                    }
+                }
+                divisor++;
+            }
+            return true;
+        }
+        public static string reverse(string input)
+        {
+
+            char[] chars = input.ToCharArray();
+
+            char[] reversed = new char[chars.Length];
+
+            for (int i = 0; i < chars.Length; i++) reversed[chars.Length - i - 1] = chars[i];
+
+            string reversedValue = new string(reversed);
+
+            return reversedValue;
+        }
+        public static bool Palindromo(string input)
+        {
+
+            if (input == reverse(input))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string Operacion(string input)
+        {
+            int num = Convert.ToInt32(input);
+
+            if (num > 0 && num < 1000000)
+            {
+                for (int i = num; i <= 10000000; i++)
+                {
+                    if (Palindromo(i.ToString()))
+                    {
+                        if (Primo(i))
+                        {
+                            return "El resultado es:" + i;
+                        }
+                    }
+                }
+                return "No hay ningun numero que cumpla los requisitos";
+
+
+            }
+            else
+            {
+                return "Ingrese un número entre 1 y 1000000";
+            }
+
+        }
+    }
+}
